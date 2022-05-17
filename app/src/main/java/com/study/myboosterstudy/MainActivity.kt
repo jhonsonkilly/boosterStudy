7045892f74c31ad7ab8335c4244707d826a59011
zhoujw <zhoujw@zenmen.com>
2022-05-18 01:49:19 +0800

    测试e

--- app/src/main/java/com/study/myboosterstudy/MainActivity.kt
+++ app/src/main/java/com/study/myboosterstudy/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main1)
+        setContentView(R.layout.activity_main2)
+        (sun.rmi.runtime.Log)
     }
 }
